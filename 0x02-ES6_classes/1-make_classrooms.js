7217f43ba8da15840e6239e550f794c0706d94cc
OlivetEkeh <ekehsons@gmail.com>
2024-07-10 18:05:41 +0100

    Popping stash

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,11 +1,6 @@
-import ClassRoom from './0-classroom.js';
+import ClassRoom from './0-classroom';
 
 export default function initializeRooms() {
-  const rooms = [
-    new ClassRoom(19),
-    new ClassRoom(20),
-    new ClassRoom(34),
-  ];
-
-  return rooms;
+  return [new ClassRoom(19), new ClassRoom(20), new ClassRoom(34)];
 }
+
