f24274d31989292431e3aa41d5105a2af7866628
OlivetEkeh <ekehsons@gmail.com>
2024-10-22 00:06:57 +0100

    added the full server directory

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,22 +1,41 @@
-/* eslint-disable */
 const fs = require('fs');
 
-const countStudents = (path) => {
-    try {
-        let data = fs.readFileSync(path, 'utf8').toString().split('\n');
-        data = data.slice(1, data.length - 1);
-        const subjects = {};
-        for (const row of data) {
-            const student = row.split(',');
-            if(!subjects[student[3]]) subjects[student[3]] = [];
-            subjects[student[3]].push(student[0]);
-        }
-        for (const subject in subjects) {
-            if (subject) console.log(`Number of students in ${subject}: ${subjects[subject].length}. List: ${subjects[subject].join(', ')}`);
-        }
+function countStudents(fileName) {
+  const students = {};
+  const fields = {};
+  let total = 0;
 
-    }catch (error) {
-        throw new Error ('Cannot load the database');
+  try {
+    const fileContents = fs.readFileSync(fileName, 'utf-8');
+    const lines = fileContents.toString().split('\n');
+    for (let u = 0; u < lines.length; u += 1) {
+      if (lines[u]) {
+        total += 1;
+
+        const field = lines[u].toString().split(',');
+        if (Object.prototype.hasOwnProperty.call(students, field[3])) {
+          students[field[3]].push(field[0]);
+        } else {
+          students[field[3]] = [field[0]];
+        }
+        if (Object.prototype.hasOwnProperty.call(fields, field[3])) {
+          fields[field[3]] += 1;
+        } else {
+          fields[field[3]] = 1;
+        }
+      }
     }
-};
+
+    const l = total - 1;
+    console.log(`Number of students: ${l}`);
+    for (const [key, value] of Object.entries(fields)) {
+      if (key !== 'field') {
+        console.log(`Number of students in ${key}: ${value}. List: ${students[key].join(', ')}`);
+      }
+    }
+  } catch (error) {
+    throw Error('Cannot load the database');
+  }
+}
+
 module.exports = countStudents;
